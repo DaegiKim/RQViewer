77aca78da4d37b77989c1333a4af6f39397c8658
DaegiKim <rmrhtdms@gmail.com>
2013-07-10 14:06:32 -0400

    init

--- src/kr/ac/inha/itlab/daegikim/Form.java
+++ src/kr/ac/inha/itlab/daegikim/Form.java
@@ -1,24 +1,22 @@
 package kr.ac.inha.itlab.daegikim;
 
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.*;
 import java.sql.*;
-import java.util.Random;
-import java.util.Vector;
+import java.util.*;
 
 public class Form extends JFrame implements ActionListener,Runnable {
     private volatile boolean isStop = false;
+    private boolean isEmpty = false;
     private boolean isDebug = true;
     private JButton btnRefresh = null;
     private JButton btnStopOrResume = null;
     private JPanel panSouth = null;
     private JTable table = null;
     private Connection conn = null;
-    private int r = 0;
-    private int g = 0;
-    private int b = 0;
 
     public Form()
     {
@@ -43,7 +41,6 @@ public class Form extends JFrame implements ActionListener,Runnable {
         panSouth = new JPanel();
         table = new JTable();
 
-
         panSouth.add(setLookAndFeel(btnRefresh));
         panSouth.add(setLookAndFeel(btnStopOrResume));
 
@@ -57,7 +54,7 @@ public class Form extends JFrame implements ActionListener,Runnable {
         this.setAlwaysOnTop(true);
         this.setLocationByPlatform(true);
 
-        setBounds(0,0,1600,180);
+        setBounds(0,0,1600,120);
         setVisible(true); // make frame visible
         new ComponentMover(this, this);
     }
@@ -115,6 +112,8 @@ public class Form extends JFrame implements ActionListener,Runnable {
             con.remove(table.getTableHeader());
 
             table = new JTable(buildTableModel(rs));
+            table.getTableHeader().setEnabled(false);
+            table.setEnabled(false);
 
             setLookAndFeel(btnRefresh);
             setLookAndFeel(btnStopOrResume);
@@ -128,17 +127,10 @@ public class Form extends JFrame implements ActionListener,Runnable {
     }
 
     public JComponent setLookAndFeel(JComponent com){
-        if(com.equals(table.getTableHeader())||com.equals(btnRefresh)||com.equals(btnStopOrResume)){
-            com.setFont(new Font("Malgun Gothic", Font.BOLD, 14));
-        }
-        else{
-            com.setFont(new Font("Malgun Gothic", Font.PLAIN, 12));
-        }
-
+        com.setFont(new Font("Malgun Gothic", Font.TRUETYPE_FONT|Font.PLAIN, 12));
         com.setBackground(Color.BLACK);
-        com.setForeground(new Color(r,g,b));
-        com.setBorder(BorderFactory.createLineBorder(new Color(r,g,b)));
-
+        com.setForeground(Color.ORANGE);
+        com.setBorder(BorderFactory.createLineBorder(Color.ORANGE));
 
         return com;
     }
@@ -160,16 +152,36 @@ public class Form extends JFrame implements ActionListener,Runnable {
             }
             data.add(vector);
         }
+
+        if(data.isEmpty()){
+            if(isEmpty==false){ Alarm(); }
+            isEmpty = true;
+        }
+        else{
+            isEmpty = false;
+        }
+
         return new DefaultTableModel(data, columnNames);
+    }
+
+    public void Alarm(){
+        try {
+            panSouth.setBackground(Color.ORANGE);
+            Thread.sleep(100);
+            panSouth.setBackground(Color.BLACK);
+            Thread.sleep(100);
+            panSouth.setBackground(Color.ORANGE);
+            Thread.sleep(100);
+            panSouth.setBackground(Color.BLACK);
+        } catch (InterruptedException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
     }
 
     @Override
     public void run() {
         while(true){
             while(isStop == false){
-                r = new Random().nextInt(255)+1;
-                g = new Random().nextInt(255)+1;
-                b = new Random().nextInt(255)+1;
                 getData();
                 System.gc();
                 try {
